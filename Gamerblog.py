7e9319d97369904af454c0d829801b147bbceb0c
Vedad <veda0039@gmail.com>
2023-11-01 09:04:24 +0100

    lad os se

--- Gamerblog.py
+++ Gamerblog.py
@@ -9,14 +9,14 @@ import json
 app = Flask(__name__)
 
 
-varer = []
+users= []
 
 
 def updateJson():
-    with open('varer.json','w+') as varefil:
-        varefil.write(json.dumps(varer))
+    with open('users.json','w+') as usersfile:
+        usersfile.write(json.dumps(users))
 def fixShitPlease():
-    for i, thing in enumerate(varer):
+    for i, thing in enumerate(users):
         thing["id"]=i
     #global varer
     #varer = varer[:]
@@ -24,25 +24,19 @@ def fixShitPlease():
         
 
 try:
-    with open('varer.json','r') as varefil:
-        varer = json.load(varefil)
+    with open('users.json','r') as usersfile:
+        users = json.load(usersfile)
         fixShitPlease()
 
 except:
     print("Filen havde ikke relevant indhold")
-    varer = []
+    users = []
 
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", varer = varer)
-
-@app.route("/buy")
-def buy():
-    item = int(request.args['item'])
-    return render_template("buy.html", vare=varer[item])
-    
+    return render_template("index.html", users = users)
 
 
 @app.route("/add_new", methods=["post", "get"])
@@ -64,31 +58,31 @@ def add_new():
         #Ret/rediger en vare
         if skabellerslet == "rediger":
             id = int(id)
-            if id > len(varer):
+            if id > len(users):
                 print("How den varer fandtes ikke!!")
                 return redirect("/")
-            vare = {'id': id, 'pris':pris, 'navn': navn}
-            varer[id] = vare
-            print(f"Varer: {varer}")
+            user = {'id': id, 'un':pris, 'pw': navn}
+            users[id] = user
+            print(f"Users: {users}")
             fixShitPlease()
             return redirect("/")
         
         #Lav en ny vare
         elif skabellerslet == "skab":
-            vare = {'id': len(varer), 'pris':pris, 'navn': navn}
-            varer.append(vare)
-            print(f"Varer: {varer}")
+            user = {'id': len(users), 'un':pris, 'pw': navn}
+            users.append(user)
+            print(f"Users: {users}")
             fixShitPlease()
             return redirect("/")
         
         #Slet varen
         else:
             print("slet", id)
-            del varer[int(id)]
+            del users[int(id)]
             fixShitPlease()
             return redirect("/")
     else:
-        return render_template("add_new.html", varer = varer )
+        return render_template("add_new.html", users = users )
 
 if __name__ == "__main__":
     app.run(debug=True, port=6002)
